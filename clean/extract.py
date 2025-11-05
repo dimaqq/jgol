b88afe3721e48d778bf42be3046b66e75aae1126
Dima Tisnek <dimaqq@gmail.com>
2025-11-05 13:36:57 +0100

    wip

--- clean/extract.py
+++ clean/extract.py
@@ -1,6 +1,9 @@
 import pathlib
 import re
 
+import numpy as np
+from PIL import Image
+
 
 def find_files(where):
     rv = []
@@ -27,26 +30,54 @@ def process_file(path, n):
         rv.append((time, m["board"]))
     return rv
 
+CONV = {
+    (".", "0"): 0,
+    (".", "."): tuple,  # not allowed
+    (".", "1"): 255,
+    ("0", "0"): 0,
+    ("0", "."): 100,
+    ("0", "1"): 255,
+    ("1", "0"): 0,
+    ("1", "."): 155,
+    ("1", "1"): 255,
+}
+
+def grayblack(data):
+    last = None
+    for d in data:
+        if last is None:
+            assert "." not in d
+            last = d
+        yield bytes(255 - CONV[it] for it in zip(last, d))
+
+
 def expand_contract(data):
     try:
         it = iter(data)
         ct, cb = next(it)
-        yield ct, cb
+        yield cb
         while True:
             t, b = next(it)
             # 0, cat; 0, dog --> 0, cat
             if t == ct: continue
             # 0, cat; 2, dog --> 0, cat; 1, cat; 2, dog
             for i in range(ct + 1, t):
-                yield i, cb
-            yield t, b
+                yield cb
+            yield b
             ct, cb = t, b
     except StopIteration:
         return
 
 
+def save_gif(data, path):
+    side = int(len(data[0]) ** 0.5)
+    frames = [Image.frombytes("L", (side, side), frame).resize((side*10, side*10), resample=0) for frame in data]
+    frames[0].save(path, save_all=True, append_images=frames[1:], duration=1000, loop=0, disposal=2)
+
 if __name__ == "__main__":
     for k, n, p in find_files("../data"):
         print("fyi", k, n)
-        for t, b in expand_contract(process_file(p, n)):
-            print(t, b)
+        frames = tuple(grayblack(expand_contract(process_file(p, n))))
+        save_gif(frames, f"{k}-{n}.gif")
+        for b in frames:
+            print(b)
